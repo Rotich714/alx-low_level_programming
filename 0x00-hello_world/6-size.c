b99e4ff816d111c1fdc348f56e71ec254101eb5b
Rotich714 <nixonrotich69@gmail.com>
2023-03-21 00:52:52 -0700

    Task 6.5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,17 @@
  *
  * Return: 0
 */
+
 int main(void)
 {
 	char a;
+
 	int b;
+
 	long int c;
+
 	long long int d;
+
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
