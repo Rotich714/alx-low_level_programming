df7bf458fa35a7ea539dfd502f836dc3c3432ef7
Rotich714 <nixonrotich69@gmail.com>
2023-03-20 17:06:27 -0700

    Task 4.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
  * 
- * main - line a line of code using puts
+ * main - a line of code using puts
  *
  * Return: 0 (Success)
-*/
+ */
 int mai(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
