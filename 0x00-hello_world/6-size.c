3b7f96a889b39153bf7631493e66a7880cd756c3
Rotich714 <nixonrotich69@gmail.com>
2023-03-21 01:11:37 -0700

    Task 6.6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,7 +21,7 @@ int main(void)
 
 	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c))
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 
